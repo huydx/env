fdccb56477834f4d6a7a7945d69eb75eff66cfe7
huydx <doxuanhuy@gmail.com>
2020-09-09 20:12:05 +0900

    add logid

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ cp ./neovim/init.vim ~/.config/nvim/init.vim
 echo "enable F key for keychron..."
 echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
 
+echo "install sdkman..."
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+echo "need to install logid follow https://github.com/PixlOne/logiops"
+sudo cp ./mouse/logid.cfg /etc
